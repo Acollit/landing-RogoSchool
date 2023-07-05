6190423ad399a64ee0ec50fd882ceff644f1ebd6
Acolit <82833189+Xareks@users.noreply.github.com>
2023-07-05 17:51:14 +0400

    Add files via upload

---
+++ src/js/_components.js
@@ -0,0 +1,10 @@
+import './components/smooth-scroll';
+import './components/tooltip';
+import './components/modals';
+import './components/slider';
+import './components/validation';
+import './components/tabs';
+import './components/custom-scroll';
+
+
+
